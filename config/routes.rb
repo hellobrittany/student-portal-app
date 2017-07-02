acb704b43820a56df7b132dbfca1f011c346078e
hellobrittany <hello@brittanywetzel.com>
2017-07-02 11:58:53 -0500

    added resume action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/' => 'sessions#index'
+
+  get '/resumes/:id' => 'resumes#show'
+  get '/resumes/:id/edit' => 'resumes#edit'
+  patch '/resumes/:id' => 'resumes#update'
+
+  
 end
