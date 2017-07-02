51f4e9c5941e94c01dfc14386c18e578ca3b657b
hellobrittany <hello@brittanywetzel.com>
2017-07-02 11:26:26 -0500

    added vue

--- app/assets/javascripts/sessions.js
+++ app/assets/javascripts/sessions.js
@@ -1,2 +1,8 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+document.addEventListener("DOMContentLoaded", function(event) { 
+  var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello Vue!'
+    }
+  });
+});
