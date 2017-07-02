4fcd17be20cc06401f592e807eaffc541a9d9f08
Zoe Lembeck <zlembeck@gmail.com>
2017-07-02 11:58:45 -0500

    added login routes controllers and view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,26 @@
 class SessionsController < ApplicationController
-	def index
-		
-	end
+def new
+  
+end
+
+def create
+  user = User.find_by(email: params[:email])
+  if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
+    flash[:success] = 'Succesfully logged in!'
+    redirect_to "/users/#{user.id}"
+  else
+    flash[:warning] = 'Invalid email or password'
+    redirect_to '/login'
+  end
+end
+
+def destroy
+    session[:user_id] = nil
+    flash[:success] = 'Successfully logged out!'
+    redirect_to '/login'
+  end
+end
+
+
 end
