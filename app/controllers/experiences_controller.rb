4a28a32ef45de2c5c1c673f727aa3c1fa33aca48
mothermoose <melanie.ilseman@gmail.com>
2017-07-03 21:25:29 -0500

    added params to update actoion of experience controller

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -10,6 +10,17 @@ class ExperiencesController < ApplicationController
 
  def update
   @experiences = Unireset.patch('https://secure-ravine-87574.herokuapp.com/api/v1/students/#{ @current_user.id}/experiences.json').body
+      experiences.assign_attributes(
+                                    start_date: params[:start_date],
+                                    end_data: params[:end_data],
+                                    job_title: params[:job_title],
+                                    company_name: params[:company_name],
+                                    details: params[:details]
+                                    )
+      if experiences.save 
+        flash[:success] = "Experience successfully updated"
+        redirect_to '/'
+      end
  end 
  
 end
