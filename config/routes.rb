8afe451ec4a0cfd1deb7aaee6b5871f435b9340f
Zoe Lembeck <zlembeck@gmail.com>
2017-07-03 21:18:49 -0500

    added edit update destroy routes controllers and show view for educations

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   
   get '/users/:id' => 'users#show' 
   delete '/users:id' => 'users#destroy'
-  
+
   get '/capstones/:id' => 'capstones#show'
   get '/capstones/:id/edit' => 'capstones#edit'
   patch '/capstones/:id' => 'capstones#update'
@@ -16,4 +16,9 @@ Rails.application.routes.draw do
   get '/resumes/:id' => 'resumes#show'
   get '/resumes/:id/edit' => 'resumes#edit'
   patch '/resumes/:id' => 'resumes#update'
+
+  
+  get '/educations/:id/edit' => 'educations/edit'
+  patch '/educations/:id' => 'educations#update'
+  delete '/educations/:id' => 'educations#destroy''
 end
