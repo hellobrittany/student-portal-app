9a5cf1e07d66564e03a84f8453866c76c2c6ff29
hellobrittany <hello@brittanywetzel.com>
2017-07-03 21:25:53 -0500

    added resume information

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,5 +1,37 @@
 class ResumesController < ApplicationController
 	def show
+		@student = Unirest.get("https://secure-ravine-87574.herokuapp.com/api/v1/students/#{@current_user.id}.json").body
 		
 	end
+
+	def edit
+		student = Unirest.get("https://secure-ravine-87574.herokuapp.com/api/v1/students/#{@current_user.id}.json").body
+
+	end
+
+	def update
+		student = Unirest.patch("https://secure-ravine-87574.herokuapp.com/api/v1/students/#{@current_user.id}.json").body
+		student.assign_attributes(
+														first_name: params[:first_name], 
+														last_name: params[:last_name],
+														email: params[:email], 
+														phone_number: params[:phone_number], 
+														short_bio: params[:short_bio], 
+														linkedin_url: params[:linkedin_url], 
+														twitter_handle: params[:twitter_handle], 
+														personal_blog: params[:personal_blog], 
+														online_resume_url: params[:online_resume_url], 
+														github_url: params[:github_url], 
+														photo: params[:photo]
+														) 
+
+
+		resume.save
+			
+		
+	end
+
+
+
+
 end
